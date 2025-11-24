c47cc8abb306bd1443b0333cdac47611d4b7f58a
YuanOwO <yuan@yuanowo.xyz>
2025-11-24 10:01:29 +0800

    相對誤差

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ def compile_code():
     run(["g++", "-O2", "legendre.cpp"], check=True)
 
 
-def execute_program(n):
-    proc = Popen(["./a.exe"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = proc.communicate(input=f"{n}\n".encode())
+def execute_program(t, n):
+    proc = Popen(["./a.out"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = proc.communicate(input=f"{t} {n}\n".encode())
 
     # errors = list(map(float, stdout.decode().strip().splitlines()))
     errors = stdout.decode().strip().splitlines()
@@ -21,14 +21,15 @@ if __name__ == "__main__":
     results = {}
 
     for n in [2, 3, 4]:
-        results[n] = execute_program(n)
+        print(f"Running for n={n}...")
+        results[n] = execute_program("n", n)
 
     with open("results_n.json", "w") as f:
         json.dump(results, f, indent=2)
 
-    # for m in [4, 16, 64]:
-    #     print(f"Running for m={m}...")
-    #     results[m] = execute_program(m)
+    for m in [4, 16, 64]:
+        print(f"Running for m={m}...")
+        results[m] = execute_program("m", m)
 
-    # with open("results_m.json", "w") as f:
-    #     json.dump(results, f, indent=2)
+    with open("results_m.json", "w") as f:
+        json.dump(results, f, indent=2)
